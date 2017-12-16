6985ad67032d341ac00cb3eccce1bfcf47242e2a
Ben Gottlieb <ben@standalone.com>
2017-12-15 21:58:02 -0600

    tweaks for latest Xcode

---
+++ PocketSocketFramework-Mac/PocketSocketFramework-Mac.h
@@ -0,0 +1,19 @@
+//
+//  PocketSocketFramework-Mac.h
+//  PocketSocketFramework-Mac
+//
+//  Created by Ben Gottlieb on 3/29/17.
+//  Copyright © 2017 Zwopple Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PocketSocketFramework-Mac.
+FOUNDATION_EXPORT double PocketSocketFramework_MacVersionNumber;
+
+//! Project version string for PocketSocketFramework-Mac.
+FOUNDATION_EXPORT const unsigned char PocketSocketFramework_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PocketSocketFramework_Mac/PublicHeader.h>
+
+
---
+++ PocketSocketFramework/PocketSocketFramework.h
@@ -0,0 +1,19 @@
+//
+//  PocketSocketFramework.h
+//  PocketSocketFramework
+//
+//  Created by Ben Gottlieb on 3/29/17.
+//  Copyright © 2017 Zwopple Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PocketSocketFramework.
+FOUNDATION_EXPORT double PocketSocketFrameworkVersionNumber;
+
+//! Project version string for PocketSocketFramework.
+FOUNDATION_EXPORT const unsigned char PocketSocketFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PocketSocketFramework/PublicHeader.h>
+
+
